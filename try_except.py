468cb8b2274a95c8decb51f031467fb0444cc053
Pelle Hanses <pelle@masendata.com>
2024-07-10 10:56:51 +0200

    Create try_except.py
    Handles errors in a function. Here an example when try to read from file.

---
+++ try_except.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+try_except.py
+Handles failures in a function.
+"""
+
+def read_file(file_pathi, verbose=1):
+    """
+    Function to read a file and print its contents.
+    If the file does not exist or other errors occur, an error message is printed.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print(content)
+            return content
+    except FileNotFoundError:
+        if verbose > 0:
+            print(f"Error: The file '{file_path}' does not exist.")
+        return f"Error: The file '{file_path}' does not exist."
+    except IOError:
+        if verbose > 0:
+            print(f"Error: An IOError occurred while trying to read the file '{file_path}'.")
+        return f"Error: An IOError occurred while trying to read the file '{file_path}'."
+    except Exception as e:
+        if verbose > 0:
+            print(f"An unexpected error occurred: {e}")
+        return f"An unexpected error occurred: {e}"
+
+## Start
+file_path = 'not_your_file.txt'
+result = read_file(file_path, 0)
+print("")
+print(f"The result: {result}")
+print("")
